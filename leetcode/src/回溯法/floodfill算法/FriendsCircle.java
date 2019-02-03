88546ce29eeacace791b8e86f56706bbdcb94648
liuqingxian1994 <377253672@qq.com>
2019-02-03 17:49:17 +0800

    201902031750

---
+++ leetcode/src/回溯法/floodfill算法/FriendsCircle.java
@@ -0,0 +1,27 @@
+package ªÿÀ›∑®.floodfillÀ„∑®;
+
+public class FriendsCircle {
+	private int n;
+
+	public int findCircleNum(int[][] M) {
+	    n = M.length;
+	    int circleNum = 0;
+	    boolean[] hasVisited = new boolean[n];
+	    for (int i = 0; i < n; i++) {
+	        if (!hasVisited[i]) {
+	            dfs(M, i, hasVisited);
+	            circleNum++;
+	        }
+	    }
+	    return circleNum;
+	}
+
+	private void dfs(int[][] M, int i, boolean[] hasVisited) {
+	    hasVisited[i] = true;
+	    for (int k = 0; k < n; k++) {
+	        if (M[i][k] == 1 && !hasVisited[k]) {
+	            dfs(M, k, hasVisited);
+	        }
+	    }
+	}
+}
