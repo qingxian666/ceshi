30d0fc117ffd3ff705a633211ddd5760c11b33ab
liuqingxian1994 <377253672@qq.com>
2019-02-04 18:08:00 +0800

    201902041807

---
+++ leetcode/src/回溯法/LetterCombinstions.java
@@ -0,0 +1,31 @@
+package »ØËÝ·¨;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinstions {
+	private static final String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+	public List<String> letterCombinations(String digits) {
+	    List<String> combinations = new ArrayList<>();
+	    if (digits == null || digits.length() == 0) {
+	        return combinations;
+	    }
+	    doCombination(new StringBuilder(), combinations, digits);
+	    return combinations;
+	}
+
+	private void doCombination(StringBuilder prefix, List<String> combinations, final String digits) {
+	    if (prefix.length() == digits.length()) {
+	        combinations.add(prefix.toString());
+	        return;
+	    }
+	    int curDigits = digits.charAt(prefix.length()) - '0';
+	    String letters = KEYS[curDigits];
+	    for (char c : letters.toCharArray()) {
+	        prefix.append(c);                         // Ìí¼Ó
+	        doCombination(prefix, combinations, digits);
+	        prefix.deleteCharAt(prefix.length() - 1); // É¾³ý
+	    }
+	}
+}
