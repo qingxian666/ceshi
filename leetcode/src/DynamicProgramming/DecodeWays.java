6fbbe526ea50681e0b0eabd63e14d8aef44b75f0
liuqingxian1994 <377253672@qq.com>
2019-02-21 22:17:32 +0800

    201902212217

---
+++ leetcode/src/DynamicProgramming/DecodeWays.java
@@ -0,0 +1,20 @@
+package DynamicProgramming;
+
+public class DecodeWays {
+	 public int numDecodings(String s) {
+	       if (s.isEmpty() || s.charAt(0) == '0') return 0;
+	        int prev = 0;
+	        int cur = 1;
+	        // 长度为n的字符串，有 n+1个阶梯
+	        for (int i = 1; i <= s.length(); ++i) {
+	        if (s.charAt(i-1) == '0') cur = 0;
+	        if (i < 2 || !(s.charAt(i - 2) == '1' ||
+	        (s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6')))
+	        prev = 0;
+	        int tmp = cur;
+	        cur = prev + cur;
+	        prev = tmp;
+	        }
+	        return cur;
+	    }
+}
